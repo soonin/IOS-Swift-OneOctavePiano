8f99269787f532174d68e8abfe10dc89d3541b47
Pooya Hatami <pooya@node-1w7jr9qro06sw04hcipbybhw2.ipv6.telus.net>
2018-08-23 04:19:24 -0700

    done!

--- IOS-Swift-OneOctavePiano/ViewController.swift
+++ IOS-Swift-OneOctavePiano/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var audioPlayer = AVAudioPlayer()
+
+    @IBOutlet var notes: [UIButton]!
+    let references = ["C3", "Cc", "D", "Dd", "E", "F", "Ff", "G", "Gg", "A", "Aa", "B", "C4"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func notePressed(_ sender: UIButton) {
+        play(note: notes.index(of: sender)! + 1 )
     }
-
-
+ 
+    
+    
+    func play(note: Int) {
+        print(note,references[note - 1])
+        let reference = references[note - 1]
+        if let path = Bundle.main.path(forResource: reference, ofType: "wav") {
+            let url = URL(fileURLWithPath: path)
+            print(reference)
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: url)
+                audioPlayer.prepareToPlay()
+                audioPlayer.play()
+                
+            }
+            catch {
+                print(error)
+            }
+        }
+    }
+    
 }
 
